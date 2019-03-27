22cb84cd3e02d2adb500d58312b7db1dbcdd5c42
lukaku98 <alvarezlucas201@gmail.com>
2019-03-26 21:55:18 -0300

    Add files via upload

---
+++ clase26_3.1/src/clase26_3/pkg1/operaciones_aritmeticas.java
@@ -0,0 +1,47 @@
+
+package clase26_3.pkg1;
+import java.util.Scanner;
+public class operaciones_aritmeticas {
+
+    
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        
+        System.out.println("ingrese valor");
+        int num1=entrada.nextInt();
+        System.out.println("ingrese el segundo valor");
+        int num2=entrada.nextInt();
+        
+        int suma= num1 + num2;
+        System.out.println("el resultado de la suma es :"+ suma);
+        
+        //punto (A)
+       //  int num1=15, num2=14, num3=3, num4=7;declaracion de variable 
+        //int resultado= (num1*num2)-(num3*num4);
+        
+       // System.out.println("el resultado es: "+resultado);//
+       
+       //punto (B)
+      // int num1=-4,num2=5,num3=2;
+       //int resultado=num1*num2*num3;
+        //System.out.println("el resultado es: "+ resultado);
+        
+        //punto (C)
+         //int num1=24, num2=2, num3=6, num4=4;
+           //      int resultado=(num1+(num2*num3))/num4;
+             //    System.out.println("el resultado es: "+ resultado);
+             
+             //punto (D)
+            // int A=1, B=2;
+             //int resultado=(A/A)/(A*B);
+            // System.out.println("el resultado es: "+ resultado);
+            
+             
+                 
+        
+       
+       
+        
+    }
+    
+}
---
+++ clase26_3/src/clase26_3/operaciones.java
@@ -0,0 +1,16 @@
+
+package clase26_3;
+
+
+public class operaciones {
+
+    public static void main(String[] args) {
+        int num1=15;//declaracion de variable
+        int num2=1358;
+        int suma= num1+num2;
+        System.out.println("el resultado es: "+ suma);
+     
+        
+    }//cierre de metodo main
+    
+}
